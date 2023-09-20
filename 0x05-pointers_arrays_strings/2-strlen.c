e33f1a3e464627731c4b1a87ae93cb38c105b1f8
Cryzklef <Okoyecrystalchuka270@gmail.com>
2023-09-20 14:16:12 -0700

    llo

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _srlen - Find the length of string
- * @s; stringd to be counted
+ * _strlen - Find the length of string
+ * @s; strings to be counted
  * Return: Length of the string
  */
 
 int _strlen(char *s)
 {
 	int i;
-	int count=0;
+	int count = 0;
 
 	for(i = 0 ; s[i] != '\0' ; i++)
 		count++;
-	return(count);
+	return (count);
 
 }
